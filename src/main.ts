fa9719e5ad3861701c1a563b1fc4377a505cf2a6
Jadilson Guedes <jadilson12@gmail.com>
2020-05-29 21:01:33 -0300

    feat: atualizando o cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { environment, swagger } from 'config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+import * as cors from 'cors';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const options = new DocumentBuilder()
@@ -14,12 +16,12 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup(swagger.URI, app, document);
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
   });
+
   await app.listen(environment.PORT);
 }
 bootstrap();
